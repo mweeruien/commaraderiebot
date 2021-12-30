5eaa410441bcd2dc22317fd63bd5b3a7d7164034
mweeruien <mweeruien@gmail.com>
2021-12-31 02:54:36 +0800

    Updated messages and commands

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import requests
 import json
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-PORT = int(os.environ.get('PORT', 5000))
+PORT = int(os.environ.get('PORT', '8443'))
 TOKEN = "5066378494:AAF7F9kK6JfPjsR0rricjqhGvcrnB80zSzk"
 PAIRINGS_SHEET = "https://api.sheety.co/88b03a94afafb9c4b54898d7a83e64de/coMaraderieBot/pairings"
 POINTS_TALLY_SHEET = "https://api.sheety.co/88b03a94afafb9c4b54898d7a83e64de/coMaraderieBot/pointsTally"
@@ -33,7 +33,7 @@ def start(update, context):
 
 def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Please contact @___ for more help!')
+    update.message.reply_text('Please contact @COMaraderie for more help!')
 
 
 def nonCommand(update, context):
@@ -46,37 +46,52 @@ def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-def companion(update, context):
+def checkcompanion(update, context):
     teleHandle = "@" + update.message.from_user.username
-    output_message = "Hi " + update.message.from_user.first_name + "! These are the details of your pairing:\n\n"
     response = requests.get(PAIRINGS_SHEET) #response is a requests.Response Object
     response_json = response.json() #transforms response into json format
 
     for person in response_json["pairings"]:
         # print(person)
-        if person['teleHandle'] == teleHandle or person['telegramHandle'] == teleHandle:
-            output_message += "Name: {}\nTelegram Handle: {}\nYear: {}\nMajor: {}\nInterests: {}\nPersonality Quiz Answers: {}\nPair Code: {}\n\n".format(
-                person["name"], person["teleHandle"], person["year"], person["major"], person["interests"], person["personalityQuizAnswers"], person["pairCode"]
-            )
-
+        if person['telegramHandle'] == teleHandle:
+            output_message = "Your COMpanion is {}!\nTelegram handle: {}\nYear/Course: {}/{}\nInterests: {}\nKind of COMpanion they’re looking for : {}\n\n".format(
+                person["name"], person["teleHandle"], person["year"], person["major"], person["interests"], person["personalityQuizAnswers"])
+            break
     # print(output_message)
     update.message.reply_text(output_message)
 
-def credits(update, context):
+def checksocialxp(update, context):
     teleHandle = "@" + update.message.from_user.username
-    output_message = "Haro " + update.message.from_user.first_name + "! These are your pair's SOCial Credits (points) so far:\n\n"
     response = requests.get(POINTS_TALLY_SHEET) #response is a requests.Response Object
     response_json = response.json() #transforms response into json format
+    updatedAsOf = ""
     # update.message.reply_text(response.text)
 
     for pair in response_json["pointsTally"]:
-        # print(person)
-        if pair['firstPersonTele'] == teleHandle or pair['secondPersonTele'] == teleHandle:
-            output_message += "Point Tally: {}\nRanking: {}".format(pair["pointTally"], pair["ranking"])
+        if pair['firstPerson'] == "" and pair['secondPerson'] == "" and pair['pointTally'] == "":
+            updatedAsOf = pair['updatedAsOf']
 
-    # print(output_message)
+        if pair['firstPersonTele'] == teleHandle:
+            output_message = "You and {} have {} SOCial XP as of {}".format(pair['secondPerson'], pair['pointTally'], updatedAsOf)
+            break
+        if pair['secondPersonTele'] == teleHandle:
+            output_message = "You and {} have {} SOCial XP as of {}".format(pair['firstPerson'], pair['pointTally'], updatedAsOf)
+            break
     update.message.reply_text(output_message)
 
+def checkpaircode(update, context):
+    teleHandle = "@" + update.message.from_user.username
+    response = requests.get(PAIRINGS_SHEET) #response is a requests.Response Object
+    response_json = response.json() #transforms response into json format
+
+    for person in response_json["pairings"]:
+        # print(person)
+        if person['teleHandle'] == teleHandle:
+            output_message = "Your unique pair code with {} is {}".format(
+                person["pairName"], person["pairCode"])
+            break
+        
+    update.message.reply_text(output_message)
 
 def main():
     """Start the bot."""
@@ -91,8 +106,9 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("companion", companion))
-    dp.add_handler(CommandHandler("credits", credits))
+    dp.add_handler(CommandHandler("checkcompanion", checkcompanion))
+    dp.add_handler(CommandHandler("checksocialxp", checksocialxp))
+    dp.add_handler(CommandHandler("checkpaircode", checkpaircode))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, nonCommand))
@@ -101,15 +117,16 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
-    # updater.start_webhook(listen="0.0.0.0",
-    #                       port=int(PORT),
-    #                       url_path=TOKEN)
-    # updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
-
+    updater.start_polling() # this is to run the bot locally
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
+    
+    # updater.start_webhook(listen="0.0.0.0",
+    #                       port=int(PORT),
+    #                       url_path=TOKEN)
+    # updater.bot.setWebhook('https://comaraderiebot.herokuapp.com/' + TOKEN)
+
     updater.idle()
 
 
